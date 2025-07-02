d972e2b2eed6d459a8f434437082943c1b4dd7b9
AdrianaValeriaCruzVargas <a20233836@pucp.edu.pe>
2025-07-02 13:22:40 -0500

    Add files via upload

---
+++ PC4-nuevo.py
@@ -0,0 +1,238 @@
+# Antes de ejecutar un script de Python en Streamlit debes definir la carpeta donde se encuentra tus archivos
+# cd ruta_de_tu_carpeta 
+# o abrimos el folder desde visual Studio Code 
+
+
+# Primero creamos un entorno virtual para instalar Streamlit y otras librerías que necesitemos.
+# python -m venv .venv
+# Esto nos permite crear un entorno virtual donde instalaremos Streamlit 
+# y observaremos la página web que se está generando en este script.
+
+# Luego activamos el entorno virtual.
+# En Windows:
+# .venv\Scripts\activate
+# En MacOS/Linux:
+# source .venv/bin/activate
+
+# Acontinuación instalamos Streamlit 
+# pip install Streamlit
+
+# Este código sirve para acceder una página web en tu navegador que te brinda información sobre Streamlit.
+# Pero se ejecuta en la terminal Python de tu computadora, no en Jupyter Notebook.
+# streamlit hello
+
+# Este comando sirve para ejecutar un script de Python en Streamlit.
+# Pero se ejecuta en la terminal de tu computadora, no en Jupyter Notebook.
+# OJO: Debes antes tener instalado Streamlit en tu computadora, debes antes definir la ruta de tus archivos y 
+##     tener un script de Python (your_script.py) que quieras ejecutar en Streamlit.
+# streamlit run your_script.py
+# python -m streamlit run your_script.py
+
+# Este código sirve para hacer un primer programa en Streamlit.
+import streamlit as st
+
+# Generamos 3 páginas en la aplicación web de Streamlit.
+# Generamos una página principal, otra donde contaran su experiencia aprendiendo a programar y una tercera donde presentarán sus gráficos.
+
+# Creamos la lista de páginas
+paginas = ['Inicio', 'Experiencia', 'Gráficos']
+
+# Creamos botones de navegación tomando la lista de páginas
+pagina_seleccionada = st.sidebar.selectbox('Selecciona una página', paginas)
+
+# Generamos condicionales para mostrar el contenido de cada página
+if pagina_seleccionada == 'Inicio':
+
+    # La función st.markdown permite centrar y agrandar la letra del título de la web en Streamlit.
+    st.markdown("<h1 style='text-align: center;'>Adrianita sobrevive a Python</h1>", unsafe_allow_html=True)
+
+    # <h1 style='text-align: center;'>Nombre de tu blog</h1>: Esto es una cadena de código HTML. 
+    # La etiqueta <h1> se utiliza para el encabezado principal de una página web, y 
+    # el atributo style se utiliza para agregar estilos CSS. 
+    # En este caso, el texto está alineado al centro (text-align: center;). 
+    # Pueden agregar emojis en el texto de Markdown utilizando códigos de emoji, por ejemplo:
+    # <h1 style='text-align: center;'>Aquí escribe un nombre creativo para tu blog 📝</h1>
+    # También pueden personalizar el color del texto utilizando el atributo style, por ejemplo:
+    # <h1 style='text-align: center; color: blue;'>Nombre de tu blog</h1>
+    # El texto dentro de las etiquetas <h1> ("Aquí escribe un nombre creativo para tu blog") es el contenido del encabezado.
+
+    # unsafe_allow_html=True: Este es un argumento opcional en la función markdown. 
+    # Por defecto, streamlit no permite HTML en el texto de Markdown.
+    # Sin embargo, establecer unsafe_allow_html en True permite el uso de HTML.
+
+    # Creamos dos columnas separadas para la imagen y el texto
+    col1, col2 = st.columns(2)
+
+    # col1, col2 = st.columns(2): Esta línea está creando dos columnas en la interfaz de usuario de la aplicación web. 
+    # La función st.columns toma un número entero como argumento que especifica el número de columnas que se deben crear. 
+    # Las columnas creadas se asignan a las variables col1 y col2.
+
+    # En la primera columna colocamos la imagen de perfil
+    col1.image("foto_perfil.jpg", caption='Esta soy yo', width=300)
+
+    # col1.image("ellie.png", caption='Ellie', width=300): Esta línea está colocando una imagen en la primera columna (col1). 
+    # La función image toma como primer argumento el nombre del archivo de la imagen que se desea mostrar. 
+    # En este caso, la imagen es "ellie.png". 
+    # El argumento caption se utiliza para proporcionar una etiqueta a la imagen, 
+    # en este caso "Aquí puedes escribir una etiqueta debajo de la imagen". 
+    # El argumento width se utiliza para especificar el ancho de la imagen, en este caso 300 píxeles.
+
+    # En la segunda columna colocamos el texto: Debe contener una presentación de ustedes
+    # Deben presentarse: ¿Quién eres?, ¿De dónde eres?, ¿Qué estudias?, ¿Qué te gusta de tu carrera?, 
+    # ¿Qué te gustaría hacer en el futuro?, ¿Qué te gusta hacer en tu tiempo libre?
+
+    texto = """
+    ¡Holi! Mi nombre es Adriana Cruz pero mis amigos más intimos me llaman Adrianita, tengo 20 años, soy capricornio y estudio Publicidad en la PUCP. Me apasiona la creatividad, el diseño y la comunicación, y siempre estoy en busca de nuevas oportunidades que me permitan crecer tanto a nivel personal como profesional.
+Más allá de lo académico, me gusta involucrarme en proyectos propios que estimulen mi lado artístico, como la creación de contenido visual y el diseño de ropa. Me considero una persona sensible, comprometida y curiosa, con muchas ganas de aprender y aportar. Mi objetivo es construir un camino donde mi creatividad tenga un impacto positivo .
+
+    """
+
+    # Las comillas triples (""") en Python se utilizan para definir cadenas multilínea.
+    
+    # Mostramos el texto
+    col2.markdown(f"<div style='text-align: justify; font-size: 15px;'>{texto}</div>", unsafe_allow_html=True)
+
+    # <div style='text-align: justify; font-size: 15px;'>{texto}</div>: Esta es una cadena de código HTML. 
+    # La etiqueta <div> se utiliza para agrupar contenido en HTML. 
+    # En este caso, el texto está justificado (text-align: justify;). 
+    # El tamaño de la fuente se establece en 15 píxeles (font-size: 15px;).
+    # El texto dentro de las etiquetas <div> es la variable texto.
+    # f"": Esto es un f-string en Python.
+    # Permite insertar el valor de una variable directamente en la cadena. 
+    # En este caso, {texto} se reemplaza por el valor de la variable texto.
+
+elif  pagina_seleccionada == 'Experiencia':
+
+    # Agregamos un título
+    st.markdown("<h1 style='text-align: center;'>Mi experiencia entrando a mi era programadora :D</h1>", unsafe_allow_html=True)
+
+    # En esta sección debes describir y comentar tu experiencia aprendiendo a programar
+    # ¿Cómo te sentiste al principio?, 
+    # ¿Qué te ha enseñado la programación?, ¿Qué te gusta de programar?, 
+    # ¿Qué te gustaría hacer con la programación en el futuro? 
+
+    # Agregar un  texto para la respuesta
+    texto_2 = """
+    Mi experiencia aprendiendo a programar fue un poco ambigua y retadora. Al principio sentía mucha curiosidad y emoción, pero también preocupación y un poco de miedo. Las primeras clases me ayudaron a entender la base de esta disciplina, aunque me costaba conectar todo ese conocimiento: comprendía cómo funcionaba cada código por separado, pero no cómo unirlos.
+Fue en las prácticas en PC, y especialmente en las tareas, donde todo empezó a tener más sentido. Las tareas me dieron el tiempo y el espacio para practicar muchas veces, con un objetivo claro. Al estar enfocadas en temáticas de mi carrera, Publicidad, me emocionaba ver cómo podía personalizar los códigos y aplicarlos profesionalmente. Eso me ayudó a renovar mi motivación.
+Hubo momentos en los que me frustraba por no recordar o no entender del todo, e incluso llegué a dudar de su utilidad en mi carrera. Pero gracias a la práctica constante y al enfoque aplicado de las tareas, logré avanzar y sentirme más segura. Fue un proceso retador, pero también muy enriquecedor.
+
+    """
+
+    # Mostramos el texto
+    st.markdown(f"<div style='text-align: justify; font-size: 18px;'>{texto_2}</div>", unsafe_allow_html=True)
+
+    # <div style='text-align: justify; font-size: 15px;'>{texto_2}</div>: Esta es una cadena de código HTML.
+    # La etiqueta <div> se utiliza para agrupar contenido en HTML.
+    # En este caso, el texto está justificado (text-align: justify;).
+    # El tamaño de la fuente se establece en 15 píxeles (font-size: 15px;).
+    # El texto dentro de las etiquetas <div> es la variable texto_2.
+    # f"": Esto es un f-string en Python.
+    # Permite insertar el valor de una variable directamente en la cadena. 
+    # En este caso, {texto_2} se reemplaza por el valor de la variable texto.
+
+    # Agregamos un subtítulo para el video
+    st.markdown("<h2 style='text-align: center;'>Video mio despues de mil intentos porque mi laptop dejaba de grabar</h2>", unsafe_allow_html=True)
+    
+    # <h2 style='text-align: center;'>Aquí escribe un nombre creativo para presentar tu video</h2>: Esta es una cadena de código HTML.
+    # La etiqueta <h2> se utiliza para un encabezado de segundo nivel en una página web.
+    # El texto está centrado (text-align: center;).
+    # El texto dentro de las etiquetas <h2> ("Aquí escribe un nombre creativo para presentar tu video") es el contenido del encabezado.
+    # unsafe_allow_html=True: Este es un argumento opcional en la función markdown.
+    # Por defecto, streamlit no permite HTML en el texto de Markdown.
+    # Sin embargo, establecer unsafe_allow_html en True permite el uso de HTML.
+    # Puedes agregar emojis en el texto de Markdown utilizando códigos de emoji.
+    # Por ejemplo, puedes agregar un emoji de video 🎥 
+
+    # Agregamos un video realizado en las practicas anteriores
+    st.video("https://drive.google.com/drive/folders/15_bNed2bI2_PnJQLAELXBe76BCXD5qIv")
+
+    # st.video("https://www.youtube.com/watch?v=X_Z7d04x9-E"): Esta línea está mostrando un video en la aplicación web.
+    # La función video toma como primer argumento la URL del video que se desea mostrar.
+    # En este caso, la URL es "https://www.youtube.com/watch?v=X_Z7d04x9-E".
+    # Puedes cambiar la URL por la de tu video en YouTube o en otra plataforma de video.
+
+    # O creamos un botón para ir al enlace del video con button
+    st.markdown(f"<div style='text-align: center;'><a href='https://drive.google.com/drive/folders/15_bNed2bI2_PnJQLAELXBe76BCXD5qIv' target='_blank'><button>Ver video</button></a></div>", unsafe_allow_html=True) 
+
+    # <div style='text-align: center;'><a href='https://drive.google.com/file/d/1REvRXSu3GuGD73w8j44135MkRiezd0gP/view?usp=drive_link' target='_blank'><button>Ver video</button></a></div>:
+    # Esta es una cadena de código HTML.
+    # La etiqueta <div> se utiliza para agrupar contenido en HTML.
+    # En este caso, el contenido está centrado (text-align: center;).
+    # La etiqueta <a> se utiliza para crear un enlace.
+    # El atributo href especifica la URL a la que se dirige el enlace.
+    # En este caso, la URL es 'https://drive.google.com/file/d/1REvRXSu3GuGD73w8j44135MkRiezd0gP/view?usp=drive_link'.
+    # El atributo target='_blank' indica que el enlace se abrirá en una nueva pestaña del navegador.
+    # La etiqueta <button> se utiliza para crear un botón.
+    # El texto dentro de las etiquetas <button> ("Ver video") es el contenido del botón.
+    # unsafe_allow_html=True: Este es un argumento opcional en la función markdown.
+    # Por defecto, streamlit no permite HTML en el texto de Markdown.
+    # Sin embargo, establecer unsafe_allow_html en True permite el uso de HTML.
+    # Puedes cambiar la URL por la de tu video en YouTube o en otra plataforma de video.
+    
+else:
+
+    # Agregamos un título para la página de gráficos
+    st.markdown("<h1 style='text-align: center;'>Mis criaturas (gráficos)</h1>", unsafe_allow_html=True)
+
+    # Creamos una lista de gráficos
+    graficos = ['Gráfico de torta de estreno de series vs. peliculas', 'Gráfico de barras de cantidad de visualizaciones según el tipo de video en tik tok', 'Mapa de mis pelis favoritas']
+
+    # Creamos un cuadro de selección en la página de gráficos
+    grafico_seleccionado = st.selectbox('Selecciona un gráfico', graficos)
+
+    # El cuadro de selección se crea con la función selectbox.
+    # El primer argumento es el texto que se muestra en el cuadro de selección.
+    # El segundo argumento es una lista de opciones que se pueden seleccionar.
+    # En este caso, las opciones son los elementos de la lista graficos.
+    # La opción seleccionada se asigna a la variable grafico_seleccionado.
+    # La variable grafico_seleccionado se utiliza para mostrar el gráfico correspondiente en la aplicación web.
+    
+
+    # Mostramos el gráfico seleccionado
+    if grafico_seleccionado == 'Gráfico de torta de estreno de series vs. peliculas':
+        st.markdown("<div style='text-align: justify; font-size: 20px;'>El gráfico de torta representa la proporción entre películas y series estrenadas en Netflix, donde las películas ocupan el 69.6% del contenido y las series el 30.4%. Esto indica una predominancia de películas en la plataforma, lo que podría reflejar una estrategia de contenido enfocada en producciones cinematográficas.</div>", unsafe_allow_html=True)
+        st.image("grafico_1_series_peliculas.png", caption='Gráfico de peliculas vs. series', width=500)
+        pass
+    elif grafico_seleccionado == 'Gráfico de barras de cantidad de visualizaciones según el tipo de video en tik tok':
+        st.markdown("<div style='text-align: justify; font-size: 20px;'>Este gráfico compara las visualizaciones de diferentes tipos de videos en TikTok, como bailes, contenido informativo, noticias, colaboraciones y tendencias. La presencia de categorías como 'Baile' sugiere que los videos de baile son uno de los tipos más populares, seguidos por contenido informativo y colaborativo. Esto refleja las preferencias de los usuarios durante 2021.</div>", unsafe_allow_html=True)
+        st.image("grafico_2_tiktok.png", caption='Gráfico de visualizaciones según tipo de video en tiktok', width=500)
+        pass
+    elif grafico_seleccionado == 'mapa de mis pelis favoritas':
+        st.markdown("<div style='text-align: justify; font-size: 20px;'>El mapa muestra la ubicación geográfica del lugar de grabación de mis películas favoritas, utilizando marcadores para indicar los lugares asociados a cada película. Esto permite visualizar de manera interactiva la distribución geográfica de mis preferencias cinematográficas.</div>", unsafe_allow_html=True)
+        # Si "mapa_cusco.html" es un archivo HTML (no una imagen), debes mostrarlo con st.components.v1.html
+        import streamlit.components.v1 as components
+        with open("mapa_peliculas.html", "r", encoding="utf-8") as f:
+            html_content = f.read()
+        components.html(html_content, height=500)
+        pass
+
+    # if grafico_seleccionado == 'Gráfico de barras verticales de lenguas aisladas':
+    # st.markdown("<div style='text-align: justify; font-size: 20px;'>Aquí debe ir una breve interpretación de tu gráfico</div>", unsafe_allow_html=True)
+    # st.image("aisladas_base_datos.png", caption='Gráfico de lenguas aisladas', width=500): Esta línea está mostrando una imagen en la aplicación web.
+    # La función image toma como primer argumento el nombre del archivo de la imagen que se desea mostrar.
+    # En este caso, la imagen es "aisladas_base_datos.png".
+    # El argumento caption se utiliza para proporcionar una etiqueta a la imagen,
+    # en este caso "Gráfico de lenguas aisladas".
+    # El argumento width se utiliza para especificar el ancho de la imagen, en este caso 500 píxeles.
+
+    # elif grafico_seleccionado == 'mapa_cusco':
+    # import streamlit.components.v1 as components
+    # with open("mapa_cusco.html", "r", encoding="utf-8") as f:
+    #     html_content = f.read()
+    # components.html(html_content, height=500): Esta línea está mostrando un archivo HTML en la aplicación web.
+    # La función components.html toma como primer argumento el contenido HTML que se desea mostrar.
+    # En este caso, el contenido HTML se lee desde el archivo "mapa_cusco.html".
+    # El argumento height se utiliza para especificar la altura del contenido HTML, en este caso 500 píxeles.
+    
+    # Si no tenemos el archivo HTML, podemos agregar el código para crear el mapa de Cusco directamente en Streamlit.
+    # Primero debes crear el diccionario de coordenadas del mapa de Cusco.
+    # Luego debes crear el mapa utilizando la librería folium y streamlit-folium.
+    # pip install folium
+    # pip install streamlit-folium
+        #import folium
+        #from streamlit_folium import st_folium
+
+        # Mostrar el mapa en Streamlit
+        #st_folium(mapa_cusco, width=700, height=500)
+    
